7c0f835630fabab0b128a0d49b8675e88bb2a70e
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2022-02-22 02:33:35 +0600

    fix main code

--- solution.go
+++ solution.go
@@ -6,11 +6,11 @@ type intCustomType int
 
 func CalcSquare(sideLen float64, sidesNum intCustomType) (result float64) {
 	if sidesNum == 3 {
-		result = (math.Pow(sideLen, 2) * math.Sqrt(3)) / 4
+		return result = (math.Pow(sideLen, 2) * math.Sqrt(3)) / 4
 	} else if sidesNum == 4 {
-		result = math.Pow(sideLen, 2)
+		return result = math.Pow(sideLen, 2)
 	} else if sidesNum == 0 {
-		result = math.Pi * math.Pow(sideLen, 2)
+		return result = math.Pi * math.Pow(sideLen, 2)
 	}
-	return
+	return 0
 }
