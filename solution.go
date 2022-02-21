3821fbc97ed673ab86cf1fde121d3acc04522367
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2022-02-22 02:39:03 +0600

    Update solution go

--- solution.go
+++ solution.go
@@ -2,19 +2,19 @@ package square
 
 import "math"
 
-type intCustomType int
+type myType int
 
 var SidesTriangle myType = 3
 var SidesSquare myType = 4
 var SidesCircle myType = 0
 
-func CalcSquare(sideLen float64, sidesNum intCustomType) (result float64) {
+func CalcSquare(sideLen float64, sidesNum myType) (result float64) {
 	if sidesNum == SidesTriangle {
-		 result = (math.Pow(sideLen, 2) * math.Sqrt(3)) / 4
+		result = (math.Pow(sideLen, 2) * math.Sqrt(3)) / 4
 	} else if sidesNum == SidesSquare {
-		 result = math.Pow(sideLen, 2)
+		result = math.Pow(sideLen, 2)
 	} else if sidesNum == SidesCircle {
-		 result = math.Pi * math.Pow(sideLen, 2)
+		result = math.Pi * math.Pow(sideLen, 2)
 	}
-	return 
+	return
 }
